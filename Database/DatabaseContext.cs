00091cd210ee4138a555b1e3e85d847fe8f60635
Damhuyentrang <damtrang102@gmail.com>
2025-04-29 16:07:37 +0700

    cơ so dư lieu sql

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace BTL_nhom11_marketPC.Database
 {
     class DatabaseContext
     {
+        private static SqlConnection conn;
+
+        private static string connStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=TenCSDL;Integrated Security=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            if (conn == null || conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn = new SqlConnection(connStr);
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public static void CloseConnection()
+        {
+            if (conn != null && conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+                conn.Dispose();
+                conn = null;
+            }
+        }
     }
 }
