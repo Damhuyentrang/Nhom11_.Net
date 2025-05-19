ae925ddf4737691ec6e20c291a3c7ee77bf30be0
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-19 22:52:02 +0700

    Add files via upload

---
+++ ViewPromotion.cs
@@ -0,0 +1,21 @@
+﻿using BTL_nhom11_marketPC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_nhom11_marketPC.Views
+{
+    public interface ViewPromotion
+    {
+        string MaKhuyenMai { get; }
+        string TenKhuyenMai { get; }
+        int PhanTramGiam { get; }
+        DateTime NgayBatDau { get; }
+        DateTime NgayKetThuc { get; }
+
+        void SetPromotionList(List<Promotion> list); 
+        void ShowMessage(string message);
+    }
+}
