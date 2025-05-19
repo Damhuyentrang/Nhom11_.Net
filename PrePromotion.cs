3bed6672b7beb1f33b90479f3b3626516d5f1eb6
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-19 22:53:48 +0700

    Add files via upload

---
+++ PrePromotion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using BTL_nhom11_marketPC.Models;
+using BTL_nhom11_marketPC.Views;
+
+namespace BTL_nhom11_marketPC.Presenters
+{
+    public class PrePromotion
+    {
+        private readonly ViewPromotion _view;
+        private readonly List<Promotion> _promotionList;
+
+        public PrePromotion(ViewPromotion view)
+        {
+            _view = view;
+            _promotionList = new List<Promotion>();
+        }
+
+        public void LoadPromotion()
+        {
+            _view.SetPromotionList(_promotionList);
+        }
+
+        public void AddPromotion()
+        {
+            var km = new Promotion
+            {
+                MaKhuyenMai = _view.MaKhuyenMai,
+                TenKhuyenMai = _view.TenKhuyenMai,
+                PhanTramGiam = _view.PhanTramGiam,
+                NgayBatDau = _view.NgayBatDau,
+                NgayKetThuc = _view.NgayKetThuc
+            };
+
+            _promotionList.Add(km);
+            _view.ShowMessage("Thêm khuyến mãi thành công.");
+            LoadPromotion();
+        }
+    }
+}
---
+++ Promotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//khuyến mãi
+namespace BTL_nhom11_marketPC.Models
+{
+    public class Promotion
+    {
+        public string MaKhuyenMai { get; set; }
+        public string TenKhuyenMai { get; set; }
+        public int PhanTramGiam { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+    }
+}
