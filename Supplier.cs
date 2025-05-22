79cf69a3a8dd5b28ec3bfbd17581102772e13215
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-22 16:23:06 +0700

    Update Supplier.cs

--- Supplier.cs
+++ Supplier.cs
@@ -8,10 +8,10 @@ namespace BTL_nhom11_marketPC.Models
 {
     public class Supplier
     {
-        public string SupplierID { get; set; }
-        public string SupplierName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public string MaNCC { get; set; }
+        public string TenNCC { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
         public string Email { get; set; }
     }
 }
