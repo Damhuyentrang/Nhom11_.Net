7b637bba1562c2e63b553241ec8e616adfb86e3e
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-22 16:31:02 +0700

    Update PreManufacturer.cs

--- PreManufacturer.cs
+++ PreManufacturer.cs
@@ -12,66 +12,23 @@ namespace BTL_nhom11_marketPC.Presenters
 {
     namespace BTL_nhom11_marketPC.Presenters
     {
-        public class ManufacturerPresenter
+        public class PreManufacturer
+    {
+        private readonly IViewManufacturer view;
+        private readonly IRepository<Manufacturer> repository;
+
+        public PreManufacturer(IViewManufacturer view, IRepository<Manufacturer> repository)
         {
-            private readonly ManufacturerView _view;
-            private readonly List<Manufacturer> _manufacturerList;
-            private readonly string connectionString = "CuaHangPhanMemMayTinh";
-
-            public ManufacturerPresenter(ManufacturerView view)
-            {
-                _view = view;
-                _manufacturerList = new List<Manufacturer>();
-            }
-
-            public void LoadManufacturers()
-            {
-                _manufacturerList.Clear();
-
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    string query = "SELECT MaHSX, TenHSX FROM HangSanXuat";
-                    SqlCommand cmd = new SqlCommand(query, conn);
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        _manufacturerList.Add(new Manufacturer
-                        {
-                            ManufacturerID = reader["MaHSX"].ToString(),
-                            ManufacturerName = reader["TenHSX"].ToString()
-                        });
-                    }
-                }
-
-                _view.SetManufacturerList(_manufacturerList);
-            }
-
-            public void AddManufacturer()
-            {
-                var manufacturer = new Manufacturer
-                {
-                    ManufacturerID = _view.ManufacturerID,
-                    ManufacturerName = _view.ManufacturerName
-                };
-
-                bool result = false;
-
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    string query = "INSERT INTO HangSanXuat (MaHSX, TenHSX) VALUES (@id, @name)";
-                    SqlCommand cmd = new SqlCommand(query, conn);
-                    cmd.Parameters.AddWithValue("@id", manufacturer.ManufacturerID);
-                    cmd.Parameters.AddWithValue("@name", manufacturer.ManufacturerName);
-
-                    result = cmd.ExecuteNonQuery() > 0;
-                }
-
-                _view.ShowMessage(result ? "Added successfully." : "Failed to add.");
-                LoadManufacturers();
-            }
+            this.view = view;
+            this.repository = repository;
         }
+
+        public void LoadManufacturers()
+        {
+            var manufacturers = repository.GetAll(); 
+            view.UpdateManufacturerList(manufacturers); 
+        }
+
     }
+  }
 }
