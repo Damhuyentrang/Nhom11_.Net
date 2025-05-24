cc888cff1ba85197ee8ba82a09ebfa82e4b6bbb9
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 17:30:33 +0700

    Update ViewManufacturer.cs

--- ViewManufacturer.cs
+++ ViewManufacturer.cs
@@ -1,20 +1,14 @@
-﻿using BTL_nhom11_marketPC.Models;
+using BTL_nhom11_marketPC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using BTL_nhom11_marketPC.Models;
 
 namespace BTL_nhom11_marketPC.Views
 {
     public interface IViewManufacturer
     {
-        string MaHSX { get; set; }
-        string TenHSX { get; set; }
-
-        void UpdateManufacturerList(IEnumerable<Manufacturer> manufacturers);
-    
+        void UpdateManufacturerList(List<Manufacturer> manufacturers);
     }
 }
