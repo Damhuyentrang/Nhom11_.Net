49bea017f4418399a16bf57efc0be2a88df36799
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 17:28:18 +0700

    Update Promotion.cs

--- Promotion.cs
+++ Promotion.cs
@@ -1,17 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//models khuyến mãi
 namespace BTL_nhom11_marketPC.Models
 {
     public class Promotion
     {
-        public string MaKhuyenMai { get; set; }
-        public string TenKhuyenMai { get; set; }
-        public int PhanTramGiam { get; set; }
+        public string MaKM { get; set; }
+        public string TenKM { get; set; }
+        public decimal GiaTriKM { get; set; }
         public DateTime NgayBatDau { get; set; }
         public DateTime NgayKetThuc { get; set; }
+        public string MaKhuyenMai { get; internal set; }
+        public string TenKhuyenMai { get; internal set; }
+        public int PhanTramGiam { get; internal set; }
+        public int PhanTramKM { get; internal set; }
     }
 }
