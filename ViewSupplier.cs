d9d1d1719da9278d7b9183f7c270ba976aa04566
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 17:31:34 +0700

    Update ViewSupplier.cs

--- ViewSupplier.cs
+++ ViewSupplier.cs
@@ -1,21 +1,15 @@
-﻿using BTL_nhom11_marketPC.Models;
+using BTL_nhom11_marketPC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BTL_nhom11_marketPC.Views
 {
     public interface IViewSupplier
- {
-     string MaNCC { get; set; }
-     string TenNCC { get; set; }
-     string DiaChi { get; set; }
-     string SoDienThoai { get; set; }
-     string Email { get; set; }
-
-     void SetSupplierList(List<Supplier> suppliers);
-     void ShowMessage(string message);
- }
+    {
+        void UpdateSupplierList(List<Supplier> suppliers);
+    }
 }
