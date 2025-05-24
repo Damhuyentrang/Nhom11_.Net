c2ebaa1128236c48760f91625fa6960fbd0ead4c
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 17:31:03 +0700

    Update ViewPromotion.cs

--- ViewPromotion.cs
+++ ViewPromotion.cs
@@ -1,21 +1,22 @@
-﻿using BTL_nhom11_marketPC.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BTL_nhom11_marketPC.Models;
 
 namespace BTL_nhom11_marketPC.Views
 {
     public interface IViewPromotion
     {
+        // Thuộc tính cần thiết để thêm khuyến mãi
         string MaKhuyenMai { get; }
         string TenKhuyenMai { get; }
         int PhanTramGiam { get; }
         DateTime NgayBatDau { get; }
         DateTime NgayKetThuc { get; }
-       void SetPromotionList(List<Promotion> list); 
-       void ShowMessage(string message);
-    
+
+        // Cập nhật danh sách khuyến mãi trong view
+        void SetPromotionList(List<IViewPromotion> promotions);
+
+        // Hiển thị thông báo
+        void ShowMessage(string message);
     }
 }
