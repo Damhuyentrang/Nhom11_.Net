a774262af7ee4841452a25a3abff5412051c4fac
Nganguyen154 <nga15404@gmail.com>
2025-05-24 22:10:53 +0700

    Add files via upload

---
+++ ViewGPU.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BTL_nhom11_marketPC.Models;
+
+namespace BTL_nhom11_marketPC.Views
+{
+    public interface IViewGPU
+    {
+        void UpdateGPUList(List<GPU> gpus);
+        void UpdateHSXList(List<Manufacturer> manufacturers); 
+        void ShowError(string message);
+    }
+}
