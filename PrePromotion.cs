00fa21337b874450f7483228eb566355ab80c8a0
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 17:33:56 +0700

    Update PrePromotion.cs

--- PrePromotion.cs
+++ PrePromotion.cs
@@ -1,40 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using BTL_nhom11_marketPC.Models;
 using BTL_nhom11_marketPC.Views;
 
 namespace BTL_nhom11_marketPC.Presenters
 {
-     public class PrePromotion
- {
-     private readonly ViewPromotion _view;
-     private readonly List<Promotion> _promotionList;
+    public class PrePromotion
+    {
+        private readonly IViewPromotion _view;
+        private readonly List<Promotion> _promotionList;
 
-     public PrePromotion(ViewPromotion view)
-     {
-         _view = view;
-         _promotionList = new List<Promotion>();
-     }
+        public PrePromotion(IViewPromotion view, Database.Repositories.PromotionRepository repository)
+        {
+            _view = view;
+            _promotionList = new List<Promotion>();
+        }
 
-     public void LoadPromotion()
-     {
-         _view.SetPromotionList(_promotionList);
-     }
+        public void LoadPromotion()
+        {
+            _view.SetPromotionList(_promotionList);
+        }
 
-     public void AddPromotion()
-     {
-         var km = new Promotion
-         {
-             MaKhuyenMai = _view.MaKhuyenMai,
-             TenKhuyenMai = _view.TenKhuyenMai,
-             PhanTramGiam = _view.PhanTramGiam,
-             NgayBatDau = _view.NgayBatDau,
-             NgayKetThuc = _view.NgayKetThuc
-         };
+        public void AddPromotion()
+        {
+            var km = new Promotion
+            {
+                MaKhuyenMai = _view.MaKhuyenMai,
+                TenKhuyenMai = _view.TenKhuyenMai,
+                PhanTramGiam = _view.PhanTramGiam,
+                NgayBatDau = _view.NgayBatDau,
+                NgayKetThuc = _view.NgayKetThuc
+            };
 
-         _promotionList.Add(km);
-         _view.ShowMessage("Thêm khuyến mãi thành công.");
-         LoadPromotion();
-     }
- }
+            _promotionList.Add(km);
+            _view.ShowMessage("Thêm khuyến mãi thành công.");
+            LoadPromotion();
+        }
+
+        internal void LoadPromotions()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
