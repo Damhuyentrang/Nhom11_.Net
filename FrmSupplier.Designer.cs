1b4b0e69c27aca25edb41afa8e891042f7d64f56
phwthao05 <phuongthaophamts05@gmail.com>
2025-05-24 23:53:42 +0700

    Add files via upload

---
+++ FrmSupplier.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System;
+
+namespace BTL_nhom11_marketPC.Forms
+{
+    partial class FrmSupplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaNCC = new System.Windows.Forms.TextBox();
+            this.txtTenNCC = new System.Windows.Forms.TextBox();
+            this.dgvSupplier = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnHuybo = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupplier)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(469, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(559, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quan Ly Nha Cung Cap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ma NCC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ten NCC";
+            // 
+            // txtMaNCC
+            // 
+            this.txtMaNCC.Location = new System.Drawing.Point(362, 220);
+            this.txtMaNCC.Name = "txtMaNCC";
+            this.txtMaNCC.Size = new System.Drawing.Size(581, 31);
+            this.txtMaNCC.TabIndex = 6;
+            this.txtMaNCC.TextChanged += new System.EventHandler(this.txtMaNCC_TextChanged);
+            // 
+            // txtTenNCC
+            // 
+            this.txtTenNCC.Location = new System.Drawing.Point(362, 319);
+            this.txtTenNCC.Name = "txtTenNCC";
+            this.txtTenNCC.Size = new System.Drawing.Size(581, 31);
+            this.txtTenNCC.TabIndex = 7;
+            // 
+            // dgvSupplier
+            // 
+            this.dgvSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSupplier.Location = new System.Drawing.Point(193, 503);
+            this.dgvSupplier.Name = "dgvSupplier";
+            this.dgvSupplier.RowHeadersWidth = 82;
+            this.dgvSupplier.RowTemplate.Height = 33;
+            this.dgvSupplier.Size = new System.Drawing.Size(1114, 315);
+            this.dgvSupplier.TabIndex = 11;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(42, 960);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(158, 46);
+            this.btnThem.TabIndex = 12;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(254, 960);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(158, 46);
+            this.btnSua.TabIndex = 13;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(479, 960);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(158, 46);
+            this.btnXoa.TabIndex = 14;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(702, 960);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(158, 46);
+            this.btnLuu.TabIndex = 15;
+            this.btnLuu.Text = "Luu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnHuybo
+            // 
+            this.btnHuybo.Location = new System.Drawing.Point(940, 960);
+            this.btnHuybo.Name = "btnHuybo";
+            this.btnHuybo.Size = new System.Drawing.Size(158, 46);
+            this.btnHuybo.TabIndex = 16;
+            this.btnHuybo.Text = "Huy bo";
+            this.btnHuybo.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(1199, 960);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(158, 46);
+            this.btnThoat.TabIndex = 17;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // FrmSupplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1500, 1062);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuybo);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvSupplier);
+            this.Controls.Add(this.txtTenNCC);
+            this.Controls.Add(this.txtMaNCC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSupplier";
+            this.Text = "FrmSupplier";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupplier)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void txtMaNCC_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaNCC;
+        private System.Windows.Forms.TextBox txtTenNCC;
+        private System.Windows.Forms.DataGridView dgvSupplier;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnHuybo;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
---
+++ FrmSupplier.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using BTL_nhom11_marketPC.Database.Repositories;
+using BTL_nhom11_marketPC.Models;
+using BTL_nhom11_marketPC.Presenters;
+using BTL_nhom11_marketPC.Views;
+
+namespace BTL_nhom11_marketPC.Forms
+{
+    public partial class FrmSupplier : Form, IViewSupplier
+    {
+        private SupplierRepository repository;
+        private PreSupplier presenter;
+        private Supplier selectedSupplier;
+        private bool _isEditing;
+        public FrmSupplier()
+        {
+            InitializeComponent();
+            repository = new SupplierRepository();
+            presenter = new PreSupplier(this, repository);
+            dgvSupplier.CellClick += dgvSupplier_CellClick;
+            LockTextBoxes(true);
+        }
+
+        private void DgvSupplier_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FrmSupplier_Load(object sender, EventArgs e)
+        {
+            presenter.LoadSupplier();
+        }
+        public void UpdateSupplierList(List<Supplier> suppliers)
+        {
+            dgvSupplier.DataSource = null;
+            dgvSupplier.DataSource = suppliers;
+            dgvSupplier.Columns["MaNCC"].HeaderText = "Mã NCC";
+            dgvSupplier.Columns["TenNCC"].HeaderText = "Tên NCC";
+            
+            this.Text = "Danh sách Nha Cung Cap";
+            this.Size = new System.Drawing.Size(600, 400);
+            dgvSupplier.Columns["MaNCC"].Width = 150;
+            dgvSupplier.Columns["TenNCC"].Width = 350;
+           
+
+        }
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void dgvSupplier_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvSupplier.Rows[e.RowIndex];
+                selectedSupplier = row.DataBoundItem as Supplier;
+                if (selectedSupplier != null)
+                {
+                    txtMaNCC.Text = selectedSupplier.MaNCC;
+                    txtTenNCC.Text = selectedSupplier.TenNCC;
+                   
+                    _isEditing = true;
+                }
+                else
+                {
+                    ClearTextBoxes();
+                }
+            }
+        }
+        private void LockTextBoxes(bool isLocked)
+        {
+            txtMaNCC.ReadOnly = isLocked;
+            txtTenNCC.ReadOnly = isLocked;
+          
+        }
+        private void ClearTextBoxes()
+        {
+            txtMaNCC.Text = string.Empty;
+            txtTenNCC.Text = string.Empty;
+           
+            
+        }
+
+        private void ResetValues()
+        {
+            ClearTextBoxes();
+            _isEditing = false;
+            LockTextBoxes(true);
+        }
+        private bool CheckControls()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNCC.Text))
+            {
+                MessageBox.Show("Mã NCC không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMaNCC.Focus();
+                return false;
+            }
+
+            if (!txtMaNCC.Text.StartsWith("NCC", StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Mã NCC phải bắt đầu bằng 'NCC'!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMaNCC.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtTenNCC.Text))
+            {
+                MessageBox.Show("Tên HSX không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTenNCC.Focus();
+                return false;
+            }
+
+            if (txtMaNCC.Text.Length > 50)
+            {
+                MessageBox.Show("Mã NCC không được vượt quá 50 ký tự!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMaNCC.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            _isEditing = false;
+            ClearTextBoxes();
+            LockTextBoxes(false);
+            txtTenNCC.Focus();
+            string newMaHSX = repository.GetNextMaNCC();
+            txtMaNCC.Text = newMaHSX; // Gán giá trị mới vào textbox
+                                    // Cập nhật lại danh sách nếu cần
+            presenter?.LoadSupplier();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (!CheckControls())
+            {
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc chắn muốn lưu?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            Supplier supplier = new Supplier
+            {
+                MaNCC = txtMaNCC.Text,
+                TenNCC = txtTenNCC.Text
+            };
+            try
+            {
+                if (!_isEditing)
+                {
+                    if (repository.CheckSupplier(txtMaNCC.Text))
+                    {
+                        MessageBox.Show("Mã NCC đã tồn tại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    presenter.AddSupplier(supplier);
+                    MessageBox.Show("Thêm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if (selectedSupplier != null && selectedSupplier.MaNCC != txtMaNCC.Text && repository.CheckSupplier(txtMaNCC.Text))
+                    {
+                        MessageBox.Show("Mã HSX đã tồn tại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    presenter.UpdateSupplier(supplier);
+                    MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                ResetValues();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi lưu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (selectedSupplier == null || string.IsNullOrWhiteSpace(selectedSupplier.MaNCC))
+            {
+                MessageBox.Show("Vui lòng chọn một Nha Cung Cap để xóa!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show($"Bạn có chắc chắn muốn xóa Nha Cung Cap có mã: {selectedSupplier.MaNCC}?",
+                "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    presenter.DeleteSupplier(selectedSupplier.MaNCC);
+                    MessageBox.Show("Xóa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ResetValues();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Lỗi khi xóa: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnHuybo_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (selectedSupplier == null || string.IsNullOrWhiteSpace(selectedSupplier.MaNCC))
+            {
+                MessageBox.Show("Vui lòng chọn một Mainboard để sửa!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _isEditing = true;
+            LockTextBoxes(false);
+            txtMaNCC.ReadOnly = true;
+            txtTenNCC.Focus();
+        }
+
+        public void UpdateSupplierList(List<IViewSupplier> suppliers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
